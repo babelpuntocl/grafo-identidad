8107c9fd9265bd7c5c249040c442d7aedae68d77
“Nicolas <nicolas.venegas@gmail.com>
2021-07-28 20:07:53 -0400

    Definición de Clases
    Circulos, Lineas, Formas exteriores

--- sketch-1/sketch.js
+++ sketch-1/sketch.js
@@ -1,9 +1,4 @@
-// create outlineShape function
-// draw ellipse
-// add random weight and color
-// add hexagon
-
-const CRYSTAL_SIZE = 300
+const CRYSTAL_SIZE = 500
 const SIDES = 6
 let PALETTE = []
 
@@ -21,89 +16,14 @@ function setup() {
 }
 
 function draw() {
-  const layer = new Circles()
-  layer.render()
+  const cirlces = new Circles()
+  cirlces.render()
+
+  const simpleLines = new SimpleLines()
+  simpleLines.render()
+
+  const outlineShape = new OutlineShape()
+  outlineShape.render()
 
   //testLines()
-  outlineShape()
-  simpleLines()
-
-  // let picker = random(1)
-  // if(picker > 0.3){
-  //   outlineShape()
-  // }
-  // if(picker > 0.3){
-  //   simpleLines()
-  // }
-  // if(picker > 0.5){
-  //   circles()
-  // }
-}
-
-function outlineShape () {
-  const strokeColor = getRandomFromPalette()
-  const weight = 1 // randomSelectTwo() ? 1 : 3
-  const hexagonTrue = randomSelectTwo()
-
-  stroke(strokeColor)
-  strokeWeight(weight)
-
-  if (hexagonTrue) {
-    push()
-      translate(width/2, height/2)
-      ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)  //rect(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE) //hexagon(0, 0, CRYSTAL_SIZE / 2)
-    pop()
-  } else {
-    push()
-      translate(width/2, height/2)
-      hexagon(0, 0, CRYSTAL_SIZE / 2) //
-    pop()
-  }
-
-}
-
-function simpleLines(){
-  const stepsOut = 8
-  const numSteps = randomSelectTwo ? stepsOut : int(stepsOut*1.25) // entre 8 y 10 (8*1.25)
-  const step = (CRYSTAL_SIZE/2) / numSteps
-  const start = floor(random(0, numSteps))
-  const stop = floor(random(start,numSteps+1))
-
-  let numShapes = randomSelectTwo() ? SIDES : SIDES * 2
-  const strokeColor = getRandomFromPalette()
-  const weight = 1 //randomSelectTwo() ? 1 : 3
-  const angle = 360 / numShapes
-
-  noFill()
-  stroke(strokeColor)
-  push()
-    translate(width/2, height/2)
-    stroke(strokeColor)
-    strokeWeight(weight)
-    for (let i = 0; i < numShapes; i++) {
-      line(start*step, 0, stop*step, 0)
-      rotate(angle)
-    }
-  pop()
-}
-
-
-// reticula de verificación
-function testLines () {
-  let numShapes = randomSelectTwo() ? SIDES : SIDES * 2
-  const strokeColor = getRandomFromPalette()
-
-  noFill()
-  stroke(200) //PALETTE[0]
-  push()
-    translate(width/2, height/2)
-    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
-
-    stroke(200) //strokeColor
-    const angle = 360 / numShapes
-    for (let i = 0; i < numShapes; i++) {
-      line(0, 0, 0, CRYSTAL_SIZE / 2)
-      rotate(angle)
-    }
-  pop()
 }
